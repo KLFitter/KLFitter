b85196a37bd2b686ac44650cf719e0fc00e9e169
Johannes Erdmann <johannes.erdmann@cern.ch>
2010-12-02 13:38:17 +0000

    Minor change for minuit status codes
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/Goettingen/KLFitter/trunk@64344 28d52da6-859c-4cdc-abcc-1ec3d2c02001

--- src/Fitter.cxx
+++ src/Fitter.cxx
@@ -199,10 +199,11 @@ int KLFitter::Fitter::Fit(int index)
     {
       fMinuitStatus=509;
     }
-
-  // check if TF problem
-  if (! fLikelihood->NoTFProblem(fLikelihood->GetBestFitParameters()))
-    fMinuitStatus = 510;
+  else {
+    // check if TF problem
+    if (! fLikelihood->NoTFProblem(fLikelihood->GetBestFitParameters()))
+      fMinuitStatus = 510;
+  }
 
   // check b-tagging and calculate probability
   if (fLikelihood->FlagBTagging()) // && fLikelihood->FlagIntegrate())
