cf0426f2e9be80a205261e0c33bd9382ab073009
Leonid Serkin <leonid.serkin@cern.ch>
2012-11-13 18:57:03 +0000

    clean ttH dir
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/Goettingen/KLFitter/trunk@305491 28d52da6-859c-4cdc-abcc-1ec3d2c02001

--- examples/ttH/loader_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./loader_C.so: /work/kkroeni/root_v5.32.02/root/include/root/cintdictversion.h /work/kkroeni/root_v5.32.02/root/include/root/RVersion.h
-loader_C__ROOTBUILDVERSION= 5.32/04
