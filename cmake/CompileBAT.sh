469f1c811301bbe6840131b2b2a58237df18a34f
Knut Zoch <knut.zoch@cern.ch>
2018-02-16 14:27:18 +0100

    Added copyright notices to various new files (mostly cmake)

--- cmake/CompileBAT.sh
+++ cmake/CompileBAT.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
-
+# Copyright (c) 2009--2017, the KLFitter developer team
+#
+# This file is part of KLFitter.
+#
+# KLFitter is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# KLFitter is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with KLFitter. If not, see <http://www.gnu.org/licenses/>.
+#
+# ==========================================================
+#
 # This is a short script to download and build the BAT library. The
 # individual steps include:
 #  - Download a tar archive of BAT v0.9.4.1.
