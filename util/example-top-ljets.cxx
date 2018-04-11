39f8650604aba723eff6f7eadd9912d1ddcdac7a
Knut Zoch <knut.zoch@cern.ch>
2018-04-11 21:57:49 +0200

    Some minor formatting changes in the example

--- util/example-top-ljets.cxx
+++ util/example-top-ljets.cxx
@@ -2,11 +2,11 @@
 #include "TreeReaderTopLJets.h"
 
 // KLFitter includes
-#include "KLFitter/Fitter.h"
 #include "KLFitter/DetectorAtlas_8TeV.h"
+#include "KLFitter/Fitter.h"
 #include "KLFitter/LikelihoodTopLeptonJets.h"
-#include "KLFitter/PhysicsConstants.h"
 #include "KLFitter/Permutations.h"
+#include "KLFitter/PhysicsConstants.h"
 
 // ROOT includes
 #include "TFile.h"
@@ -15,73 +15,72 @@
 #include <iostream>
 #include <memory>
 
-int main(int argc, char *argv[]){
-  if (argc != 2){
-    std::cerr << "ERROR: Expecting 1 argument but " << argc-1 << " arguments provided. Exiting." << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "ERROR: Expecting 1 argument but " << argc - 1;
+    std::cerr << " arguments provided. Exiting." << std::endl;
     return 1;
   }
 
-  std::string base_dir = argv[1];
+  std::string base_dir{argv[1]};
 
-  // Open the input ROOT file
+  // Open the input ROOT file and check it afterwards.
   auto f_input = TFile::Open((base_dir + "/data/examples/top-ljets-input.root").c_str(), "READ");
-
-  //Check if we opened the correct file
-  if (f_input == nullptr){
+  if (f_input == nullptr) {
     std::cerr << "ERROR: Cannot open the input ROOT file. Aborting." << std::endl;
     return 1;
   }
 
-  // Create and connect the instance of TTree for reading
-  // Name of the input tree is "nominal"
+  // Create and connect the instance of TTree for reading. Name
+  // of the input tree is "nominal". Check afterwards if
+  // everything went fine.
   TTree *t = (TTree*)f_input->Get("nominal");
-
-  // Check if everything went fine
-  if (t == nullptr){
+  if (t == nullptr) {
     std::cerr << "ERROR: Cannot read 'nominal' tree from the input file. Aborting" << std::endl;
     return 1;
   }
 
-  // Store the number of entried in the input file
+  // Store the number of entries of the input file.
   const int nEntries = t->GetEntries();
 
-  // Create the instance of object that stores the event information
+  // Create an instance of the TreeReaderTopLJets that stores all
+  // event information (i.e. all variables).
   TreeReaderTopLJets event(t);
 
-  // Create new fitter
+  // Create an instance of a fitter.
   KLFitter::Fitter fitter{};
 
-  // Create detector - this is needed for transfer functions
-  // The energy in the name of the class is irrelevant
-  // it is just a dummy name
-  KLFitter::DetectorAtlas_8TeV detector{(base_dir + "/data/transferfunctions/8TeV/ttbar/mc12_LCJets_v1").c_str()}; // 8 TeV TF
-
-  if (!fitter.SetDetector(&detector)){
+  // Create a detector - this is needed for the transfer
+  // functions. If setting the detector fails, abort.
+  KLFitter::DetectorAtlas_8TeV detector{(base_dir + "/data/transferfunctions/8TeV/ttbar/mc12_LCJets_v1").c_str()};
+  if (!fitter.SetDetector(&detector)) {
     std::cerr << "ERROR: Failed to set detector! Aborting" << std::endl;
     return 1;
   }
 
-  // Create instance of likelihood
+  // Create an instance of the lepton+jets likelihood.
   KLFitter::LikelihoodTopLeptonJets likelihood{};
 
-  // Set likelihood properties
-  likelihood.PhysicsConstants()->SetMassTop(172.5); // in GeV
-  likelihood.SetBTagging(KLFitter::LikelihoodBase::BtaggingMethod::kWorkingPoint); // kNotag, kVeto, kVetoLight, kVetoBoth, kWorkingPoint
+  // Set the likelihood properties.
+  likelihood.PhysicsConstants()->SetMassTop(172.5);  // mass in GeV
+  // Possible options: kNotag, kVeto, kVetoLight, kVetoBoth, kWorkingPoint
+  likelihood.SetBTagging(KLFitter::LikelihoodBase::BtaggingMethod::kWorkingPoint);
   likelihood.SetFlagTopMassFixed(true);
   likelihood.SetLeptonType(KLFitter::LikelihoodTopLeptonJets::LeptonType::kMuon);
 
-  // Set the likelihood
-  if (!fitter.SetLikelihood(&likelihood)){
+  // Set the likelihood in the fitter instance.
+  if (!fitter.SetLikelihood(&likelihood)) {
     std::cerr << "ERROR: Failed to set likelihood. Aborting." << std::endl;
     return 1;
   }
 
   bool isFirst(true);
 
-  // open output ROOT file
+  // Open the output ROOT file.
   auto f_output = TFile::Open("top-ljets-output.root","RECREATE");
   TTree *out_tree = new TTree("KLFitter_output", "Example tree with KLFitter output");
 
+  // Declare variables to store event information.
   std::vector<float> klf_bhad_pt;
   std::vector<float> klf_bhad_eta;
   std::vector<float> klf_bhad_phi;
@@ -117,7 +116,7 @@ int main(int argc, char *argv[]){
   std::vector<char> klf_fit_parameter_at_limit;
   std::vector<char> klf_fit_invalid_transfer_function;
 
-  // prepare the output variables in a tree
+  // Prepare the output variables as branches of the tree.
   out_tree->Branch("klf_bhad_pt",&klf_bhad_pt);
   out_tree->Branch("klf_bhad_eta",&klf_bhad_eta);
   out_tree->Branch("klf_bhad_phi",&klf_bhad_phi);
@@ -153,16 +152,17 @@ int main(int argc, char *argv[]){
   out_tree->Branch("klf_fit_parameter_at_limit",&klf_fit_parameter_at_limit);
   out_tree->Branch("klf_fit_invalid_transfer_function",&klf_fit_invalid_transfer_function);
 
-  // loop over events
+  // Loop over all events in the input tree.
   std::cout << "Started looping over " << nEntries << " entries" << std::endl;
-  for (int ievent = 0; ievent < nEntries; ievent++){
-    if (ievent %100 == 0){
+  for (int ievent = 0; ievent < nEntries; ievent++) {
+    if (ievent %100 == 0) {
       std::cout << "Processing event: " << ievent << " out of " << nEntries << " events" << std::endl;
     }
-    //Read the information from the input ROOT file
+
+    //Read the information from the input ROOT file.
     event.GetEntry(ievent);
 
-    // clear all vectors for output file
+    // Clear all vectors for of the variables.
     klf_bhad_pt.clear();
     klf_bhad_eta.clear();
     klf_bhad_phi.clear();
@@ -198,43 +198,49 @@ int main(int argc, char *argv[]){
     klf_fit_parameter_at_limit.clear();
     klf_fit_invalid_transfer_function.clear();
 
-    // Create instance of particles
-
-    // here, you need to make sure that
-      // - the particles are in the range allowed by the transfer functions (eta and pt)
-      // - the energies and momenta are in GeV
-      // - be aware that *all* particles you're adding are considered in the fit
-      //   (many particles lead to many permutations to be considered and hence a long
-      //   running time and not necessarily good fitting results due to the many available
-      //   permutations)
-      // the arguments taken py AddParticle() are
-      // - TLorentzVector of the physics 4-momentum
-      // - detector eta for the evaluation of the transfer functions (for muons: just use the physics eta)
-      // - type of particle
-      // - an optional name of the particle (pass empty string in case you don't want to give your particle a name)
-      // - index of the particle in your original collection (for convenience)
+    // Create an instance of particles. You need to make sure that:
+    // - the particles are in the range allowed by the transfer
+    //   functions (eta and pt)
+    // - the energies and momenta are in GeV
+    //
+    // Be aware that *all* particles you're adding are considered
+    // in the fit (many particles lead to many permutations to be
+    // considered and hence a long running time and not
+    // necessarily good fitting results due to the many available
+    // permutations)
+    //
+    // The arguments taken py AddParticle() are:
+    // - TLorentzVector of the physics 4-momentum
+    // - detector eta for the evaluation of the transfer
+    //   functions (for muons: just use the physics eta)
+    // - type of particle an optional name of the particle (pass
+    //   empty string in case you don't want to give your
+    //   particle a name)
+    // - index of the particle in your original collection (for
+    //   convenience)
     KLFitter::Particles particles{};
 
-    // Add leptons
+    // Add leptons.
     TLorentzVector lepton;
     lepton.SetPtEtaPhiE(event.lepton_pt, event.lepton_eta, event.lepton_phi, event.lepton_e);
-    if (event.lepton_is_e){
-        likelihood.SetLeptonType(1); // set lepton type to electron
+    if (event.lepton_is_e) {
+        likelihood.SetLeptonType(1);  // set lepton type to electron
       particles.AddParticle(&lepton, event.lepton_eta, KLFitter::Particles::kElectron);
-    } else if (event.lepton_is_mu){
-        likelihood.SetLeptonType(2); // set lepton type to muon
+    } else if (event.lepton_is_mu) {
+        likelihood.SetLeptonType(2);  // set lepton type to muon
       particles.AddParticle(&lepton, event.lepton_eta, KLFitter::Particles::kMuon);
     } else {
-      std::cerr << "WARNING: Event has no electrons nor muons. Skipping." << std::endl;
+      std::cerr << "WARNING: Event has no electrons or muons. Skipping." << std::endl;
       continue;
     }
 
-    // Add jets - the input file required at least 4 jets per event and at least 1 btagged
-    for (unsigned int ijet = 0; ijet < 4; ijet++){ // we only want to consider 4 jets
+    // Add jets - the input file already required at least 4 jets
+    // per event, out of which at least 1 is b-tagged.
+    for (unsigned int ijet = 0; ijet < 4; ijet++) {
       TLorentzVector jet;
       jet.SetPtEtaPhiE(event.jet_pt->at(ijet), event.jet_eta->at(ijet),
           event.jet_phi->at(ijet), event.jet_e->at(ijet));
-      // arguments are as follows:
+      // Arguments are as follows:
       //  1) TLorentzVector of jet
       //  2) jet eta
       //  3) KLFitter particle type. kParton for jets
@@ -249,75 +255,64 @@ int main(int argc, char *argv[]){
           (int)event.jet_has_btag->at(ijet), 0.6, 145., KLFitter::Particles::kNone, event.jet_btag_weight->at(ijet));
     }
 
-    // Add particles to the likelihood
-
-    //fitter.SetParticles(p.get());
-    if (!fitter.SetParticles(&particles)){
+    // Add particles to the likelihood.
+    if (!fitter.SetParticles(&particles)) {
       std::cerr << "ERROR: Failed to add particles to KLFitter. Aborting." << std::endl;
       return 1;
     }
+
     // Add MET information
     const double met_ex = event.met_met*cos(event.met_phi);
     const double met_ey = event.met_met*sin(event.met_phi);
-
-
-    if (!fitter.SetET_miss_XY_SumET(met_ex, met_ey, event.sumet)){
+    if (!fitter.SetET_miss_XY_SumET(met_ex, met_ey, event.sumet)) {
       std::cerr << "ERROR: Failed to add MET to fitter. Aborting." << std::endl;
       return 1;
     }
 
-    // Loop over all permutations
+    // Loop over all permutations.
     const int nperm = fitter.Permutations()->NPermutations();
-    for (int iperm  = 0; iperm < nperm; iperm++){
-      // Do the fitting magic
+    for (int iperm  = 0; iperm < nperm; iperm++) {
+      // Do the fitting magic.
       fitter.Fit(iperm);
 
-      // Read the output of the fit
-      //
-      // Minuit converge status
+      // Read the output and convergence status of the fit.
       unsigned int ConvergenceStatusBitWord = fitter.ConvergenceStatus();
-      bool MinuitDidNotConverge = (ConvergenceStatusBitWord &
-        fitter.MinuitDidNotConvergeMask) != 0;
-      bool FitAbortedDueToNaN = (ConvergenceStatusBitWord &
-        fitter.FitAbortedDueToNaNMask) != 0;
-      bool AtLeastOneFitParameterAtItsLimit = (ConvergenceStatusBitWord &
-        fitter.AtLeastOneFitParameterAtItsLimitMask) != 0;
-      bool InvalidTransferFunctionAtConvergence = (ConvergenceStatusBitWord &
-        fitter.InvalidTransferFunctionAtConvergenceMask) != 0;
+      bool MinuitDidNotConverge = (ConvergenceStatusBitWord & fitter.MinuitDidNotConvergeMask) != 0;
+      bool FitAbortedDueToNaN = (ConvergenceStatusBitWord & fitter.FitAbortedDueToNaNMask) != 0;
+      bool AtLeastOneFitParameterAtItsLimit = (ConvergenceStatusBitWord & fitter.AtLeastOneFitParameterAtItsLimitMask) != 0;
+      bool InvalidTransferFunctionAtConvergence = (ConvergenceStatusBitWord & fitter.InvalidTransferFunctionAtConvergenceMask) != 0;
 
-      //Get likelihood and event probability values
-      double likelihood = fitter.Likelihood()->LogLikelihood(
-        fitter.Likelihood()->GetBestFitParameters());
-
-      // probbaility is NOT normalized!
+      // Get log likelihood and event probability values. Note
+      // that the event probablity is _not_ normalized.
+      double likelihood = fitter.Likelihood()->LogLikelihood(fitter.Likelihood()->GetBestFitParameters());
       double event_probability = std::exp(fitter.Likelihood()->LogEventProbability());
 
-      // Get the values of fitted variables
+      // Get the values of all fitted variables.
       auto modelParticles = fitter.Likelihood()->ParticlesModel();
       auto permutedParticles = fitter.Likelihood()->PParticlesPermuted();
 
-      // bhad quark
+      // Hadronic b quark.
       float bhad_pt = modelParticles->Parton(0)->Pt();
       float bhad_eta = modelParticles->Parton(0)->Eta();
       float bhad_phi = modelParticles->Parton(0)->Phi();
       float bhad_e = modelParticles->Parton(0)->E();
       unsigned int bhad_index = (*permutedParticles)->JetIndex(0);
 
-      // blep quark
+      // Leptonic b quark.
       float blep_pt = modelParticles->Parton(1)->Pt();
       float blep_eta = modelParticles->Parton(1)->Eta();
       float blep_phi = modelParticles->Parton(1)->Phi();
       float blep_e = modelParticles->Parton(1)->E();
       unsigned int blep_index = (*permutedParticles)->JetIndex(1);
 
-      //light quark 1
+      // Light quark 1.
       float lquark1_pt = modelParticles->Parton(2)->Pt();
       float lquark1_eta = modelParticles->Parton(2)->Eta();
       float lquark1_phi = modelParticles->Parton(2)->Phi();
       float lquark1_e = modelParticles->Parton(2)->E();
       unsigned int lquark1_index = (*permutedParticles)->JetIndex(2);
 
-      //light quark 2
+      // Light quark 2.
       float lquark2_pt = modelParticles->Parton(3)->Pt();
       float lquark2_eta = modelParticles->Parton(3)->Eta();
       float lquark2_phi = modelParticles->Parton(3)->Phi();
@@ -329,33 +324,31 @@ int main(int argc, char *argv[]){
       float lepton_phi = -9999;
       float lepton_e = -9999;
 
-      // always check for lepton type or the code will crash
-      if (event.lepton_is_e){
-        // electron
+      // Always check for lepton type or the code will crash.
+      if (event.lepton_is_e) {
         lepton_pt = modelParticles->Electron(0)->Pt();
         lepton_eta = modelParticles->Electron(0)->Eta();
         lepton_phi = modelParticles->Electron(0)->Phi();
         lepton_e = modelParticles->Electron(0)->E();
-      } else if (event.lepton_is_mu){
-        // muon
+      } else if (event.lepton_is_mu) {
         lepton_pt = modelParticles->Muon(0)->Pt();
         lepton_eta = modelParticles->Muon(0)->Eta();
         lepton_phi = modelParticles->Muon(0)->Phi();
         lepton_e = modelParticles->Muon(0)->E();
       }
 
-      // neutrino
+      // Neutrino parameters.
       float neutrino_pt = modelParticles->Neutrino(0)->Pt();
       float neutrino_eta = modelParticles->Neutrino(0)->Eta();
       float neutrino_phi = modelParticles->Neutrino(0)->Phi();
       float neutrino_e = modelParticles->Neutrino(0)->E();
 
-      // fill the vectors
-      // it's better/safer to store booleans as chars in ROOT files
-      klf_fit_minuit_did_not_converge.emplace_back(static_cast<char> (MinuitDidNotConverge) );
-      klf_fit_aborted_to_nan.emplace_back(static_cast<char> (FitAbortedDueToNaN) );
-      klf_fit_parameter_at_limit.emplace_back(static_cast<char> (AtLeastOneFitParameterAtItsLimit) );
-      klf_fit_invalid_transfer_function.emplace_back(static_cast<char> (InvalidTransferFunctionAtConvergence) );
+      // Fill the vectors with the output variables. Note: it's
+      // better/safer to store booleans as chars in ROOT files.
+      klf_fit_minuit_did_not_converge.emplace_back(static_cast<char>(MinuitDidNotConverge));
+      klf_fit_aborted_to_nan.emplace_back(static_cast<char>(FitAbortedDueToNaN));
+      klf_fit_parameter_at_limit.emplace_back(static_cast<char>(AtLeastOneFitParameterAtItsLimit));
+      klf_fit_invalid_transfer_function.emplace_back(static_cast<char>(InvalidTransferFunctionAtConvergence));
 
       klf_bhad_pt.emplace_back(bhad_pt);
       klf_bhad_eta.emplace_back(bhad_eta);
@@ -388,8 +381,8 @@ int main(int argc, char *argv[]){
       klf_loglikelihood.emplace_back(likelihood);
       klf_event_probability.emplace_back(event_probability);
 
-      // print some values for the first event
-      if (isFirst){
+      // Print some values for the first event.
+      if (isFirst) {
         printf("----------------------------------------------------------------------------------------------\n");
         printf("----------------------------------------Permutation %2i----------------------------------------\n",iperm);
         printf("----------------------------------------------------------------------------------------------\n");
@@ -414,18 +407,15 @@ int main(int argc, char *argv[]){
     out_tree->Fill();
   }
 
-  // Close the ROOT files
-
-  // go to output file
+  // Go to the output file and write it.
   f_output->cd();
-  // write to output file
   std::cout << std::endl << "Writing into output root file: " << "ljets_output.root" << std::endl << std::endl;
   out_tree->Write();
 
-  // close ROOT files
+  // Close both input and output ROOT files.
   f_output->Close();
   f_input->Close();
 
-  // Successful run
+  // Return 0 after a success run.
   return 0;
 }
