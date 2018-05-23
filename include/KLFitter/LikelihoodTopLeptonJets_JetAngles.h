86d932116bca276c099550d3b278514e76792e05
Knut Zoch <knut.zoch@cern.ch>
2018-05-23 18:15:31 +0200

    Revised comments in LikelihoodTopLeptonJets_JetAngles.h

--- include/KLFitter/LikelihoodTopLeptonJets_JetAngles.h
+++ include/KLFitter/LikelihoodTopLeptonJets_JetAngles.h
@@ -37,103 +37,124 @@ namespace KLFitter {
 class ResolutionBase;
 
 /**
-  * \class KLFitter::LikelihoodTopLeptonJets_JetAngles
-  * \brief Add brief description here
-  *
-  * Add a detailed description here.
+  * This is an extension of LikelihoodTopLeptonJets, which adds eta and phi
+  * values of the jets to the fit. The additional parameters need the
+  * corresponding parameterizations in the transfer functions.
   */
 class LikelihoodTopLeptonJets_JetAngles : public KLFitter::LikelihoodTopLeptonJets {
  public:
-  /** \name Constructors and destructors */
-  /* @{ */
+  /// \name Constructors and destructors
+  /// @{
 
-  /**
-    * The default constructor.
-    */
+  /// The (defaulted) constructor.
   LikelihoodTopLeptonJets_JetAngles();
 
-  /**
-    * The (defaulted) destructor.
-    */
+  /// The (defaulted) destructor.
   ~LikelihoodTopLeptonJets_JetAngles();
 
-  /* @} */
-  /** \name Member functions (Set)  */
-  /* @{ */
+  /// @}
+
+  /// Enumerator for the fitted parameters of this likelihood.
+  enum Parameters { parBhadE,     ///< Energy of the hadronic b quark
+                    parBlepE,     ///< Energy of the leptonic b quark
+                    parLQ1E,      ///< Energy of the light quark 1
+                    parLQ2E,      ///< Energy of the light quark 2
+                    parLepE,      ///< Energy of the lepton
+                    parNuPx,      ///< p_x of the neutrino
+                    parNuPy,      ///< p_y of the neutrino
+                    parNuPz,      ///< p_z of the neutrino
+                    parBhadEta,   ///< Eta of the hadronic b quark
+                    parBlepEta,   ///< Eta of the leptonic b quark
+                    parLQ1Eta,    ///< Eta of the light quark 1
+                    parLQ2Eta,    ///< Eta of the light quark 2
+                    parBhadPhi,   ///< Phi of the hadronic b quark
+                    parBlepPhi,   ///< Phi of the leptonic b quark
+                    parLQ1Phi,    ///< Phi of the light quark 1
+                    parLQ2Phi,    ///< Phi of the light quark 2
+                    parTopM       ///< Mass of the top quark
+  };
+
+  /// \name Member functions (misc)
+  /// @{
 
   /**
-    * Enumerator for the parameters.
-    */
-  enum Parameters { parBhadE, parBlepE, parLQ1E, parLQ2E, parLepE, parNuPx, parNuPy, parNuPz, parBhadEta, parBlepEta, parLQ1Eta, parLQ2Eta, parBhadPhi, parBlepPhi, parLQ1Phi, parLQ2Phi, parTopM };
-
-  /* @} */
-  /** \name Member functions (misc)  */
-  /* @{ */
-
-  /**
-    * Define the parameters of the fit.
-    */
+   * Define the parameters of the fit. This calls BCModel::AddParameter() for
+   * all parameters in the enum #Parameters. Reimplemented with respect to
+   * LikelihoodTopLeptonJets::DefineParameters() due to the additional
+   * parameters in #Parameters.
+   */
   void DefineParameters() override;
 
   /**
-    * The posterior probability definition, overloaded from BCModel.
-    * @param parameters A vector of parameters (double values).
-    * @return The logarithm of the prior probability.
-    */
+   * Identical to LikelihoodTopLeptonJets::GetInitialParametersWoNeutrinoPz(),
+   * but adds the initial values for the additional angular parameters.
+   * @return vector of initial values.
+   */
+  std::vector<double> GetInitialParametersWoNeutrinoPz() override;
+
+  /**
+   * The posterior probability definition, overloaded from BCModel. With respect
+   * to LikelihoodTopLeptonJets::LogLikelihood(), this also adds resolutions
+   * terms for the jet eta and phi values.
+   * @param parameters A vector of parameters (double values).
+   * @return The logarithm of the prior probability.
+   */
   double LogLikelihood(const std::vector <double> & parameters) override;
 
   /**
-    * The posterior probability definition, overloaded from BCModel. Split up into several subcomponents
-    * @param parameters A vector of parameters (double values).
-    * @return A vector with the components of the logarithm of the prior probability. Its components are:
-    * 0:  TF_bhad
-    * 1:  TF_blep
-    * 2:  TF_lq1
-    * 3:  TF_lq2
-    * 4:  TF_lep
-    * 5:  TF_METx
-    * 6:  TF_METy
-    * 7:  TFeta_bhad
-    * 8:  TFeta_blep
-    * 9:  TFeta_lq1
-    *10:  TFeta_lq2
-    *11:  TFphi_bhad
-    *12:  TFphi_blep
-    *13:  TFphi_lq1
-    *14:  TFphi_lq2
-    *15:  BW_Whad
-    *16:  BW_Wlep
-    *17:  BW_Thad
-    *18:  BW_Tlep
-    */
+   * The posterior probability definition, overloaded from BCModel. Instead of
+   * the final log likelihood value as in LogLikelihood(), this returns all
+   * subcomponents.
+   * @param parameters A vector of parameters (double values).
+   * @return A vector with the components of the logarithm of the prior
+   * probability. Its components are: \n
+   *   0) TF_bhad \n
+   *   1) TF_blep \n
+   *   2) TF_lq1 \n
+   *   3) TF_lq2 \n
+   *   4) TF_lep \n
+   *   5) TF_METx \n
+   *   6) TF_METy \n
+   *   7)  TFeta_bhad \n
+   *   8)  TFeta_blep \n
+   *   9)  TFeta_lq1 \n
+   *  10)  TFeta_lq2 \n
+   *  11)  TFphi_bhad \n
+   *  12)  TFphi_blep \n
+   *  13)  TFphi_lq1 \n
+   *  14)  TFphi_lq2 \n
+   *  15)  BW_Whad \n
+   *  16)  BW_Wlep \n
+   *  17)  BW_Thad \n
+   *  18)  BW_Tlep \n
+   */
   std::vector<double> LogLikelihoodComponents(std::vector <double> parameters) override;
 
-  /**
-    * Get initial values for the parameters with a dummy of "0.0" for the neutrino pz.
-    * The decision on the initial value for the neutrino pz then needs to be done in
-    * GetInitialParameters().
-    * @return vector of initial values.
-    */
-  std::vector<double> GetInitialParametersWoNeutrinoPz() override;
-
-  /* @} */
+  /// @}
 
  protected:
-  /** \name Member functions (misc)  */
-  /* @{ */
-
   /**
-    * Update 4-vectors of model particles.
-    * @return An error flag.
-    */
-  int CalculateLorentzVectors(std::vector <double> const& parameters) override;
-
-  /**
-    * Adjust parameter ranges
-    */
+   * Adjust ranges of the parameters. This either takes the parameter sigmas
+   * from the transfer functions, if #fFlagGetParSigmasFromTFs is set.
+   * Otherwise, a fixed sigma is used to calculate the parameter ranges. Then,
+   * SetParameterRange() is called to set the range. If #fFlagTopMassFixed is
+   * set, the top mass will be fixed to the pole mass. Reimplemented with
+   * respect to LikelihoodTopLeptonJets::AdjustParameterRanges() due to
+   * additional parameters in #Parameters.
+   * @return An error code.
+   */
   int AdjustParameterRanges() override;
 
-  /* @} */
+  /**
+   * Update 4-vector values of the model particles. This updates the internal
+   * variables, such as #bhad_fit_px or #thad_fit_m to the latest values from
+   * the fit. These variables are for example used in the log likelihood
+   * calculation. As opposed to
+   * LikelihoodTopLeptonJets::CalculateLorentzVectors(), the 4-vector values for
+   * the jets include the fitted angular information as well.
+   * @return An error flag.
+   */
+  int CalculateLorentzVectors(std::vector <double> const& parameters) override;
 };
 }  // namespace KLFitter
 
