0b931902177f9d937e2dcd37f50a886e5800d823
Tomas Dado <tomas.dado@cern.ch>
2018-10-11 12:50:32 +0200

    Fixing declaration

--- include/KLFitter/ResSingleGaussBase.h
+++ include/KLFitter/ResSingleGaussBase.h
@@ -77,7 +77,7 @@ class ResSingleGaussBase : public ResolutionBase {
     * @param x The value of x.
     * @return The width.
     */
-  virtual double GetSigma(double x) override;
+  virtual double GetSigma(double x) = 0;
 
   /**
     * Return the probability of the true value of x given the
