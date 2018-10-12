ea7a687a0c5a2c8ee11e5ea6d605bcae20c823d4
Tomas Dado <tomas.dado@cern.ch>
2018-10-12 16:39:11 +0200

    Marking function as override so it is clear that it overrides BAT functions

--- include/KLFitter/LikelihoodBase.h
+++ include/KLFitter/LikelihoodBase.h
@@ -300,7 +300,7 @@ class LikelihoodBase : public BCModel {
     * @param parameters A vector of parameters (double values).
     * @return The logarithm of the prior probability.
     */
-  virtual double LogAPrioriProbability(const std::vector <double> & parameters) { return 0; }
+  virtual double LogAPrioriProbability(const std::vector <double> & parameters) override { return 0; }
 
   /**
     * The posterior probability definition, overloaded from BCModel.
