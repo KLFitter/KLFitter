8ef4f458dc77c7ed29e6ae9b3458234d57feb4d7
Knut Zoch <knut.zoch@gmail.com>
2018-10-18 12:47:52 +0200

    Added executable with googletest suite

---
+++ tests/gtests.cxx
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2009--2018, the KLFitter developer team
+ *
+ * This file is part of KLFitter.
+ *
+ * KLFitter is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * KLFitter is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with KLFitter. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "gtest/gtest.h"
+
+// Include all individual tests here.
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
